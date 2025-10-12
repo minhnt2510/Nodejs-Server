9128968ec41b74fb4dad98d87e1818a08eacb806
minhnt2510 <126145785+minhnt2510@users.noreply.github.com>
2025-10-12 20:35:48 +0700

    nodejs to typescript config

--- ServerNodejs/main.js
+++ ServerNodejs/main.js
@@ -1,10 +1,9 @@
-const http = require("http");
-
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-    if (req.url === "/") res.end("Trang chủ");
-    else if (req.url === "/about") res.end("Giới thiệu");
-    else res.end("404 - Không tìm thấy");
-  })
-  .listen(3000, () => console.log("Server chạy ở http://localhost:3000"));
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+  res.send("Hello TMinh");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
